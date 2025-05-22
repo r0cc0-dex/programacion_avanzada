539edc38ccb204cc13a4ce354a78c813e6a0f8a8
r0cc0 <githubguzman@gmail.com>
2025-05-21 22:03:52 -0300

    Add files via upload

---
+++ PruebaVariables.java
@@ -0,0 +1,60 @@
+
+public class PruebaVariables {
+    
+   //1//
+    // int edad = 17 {
+   // System.out.println(edad);
+//}
+   //2//
+   //double precioProducto = 45.99;
+
+   //3//
+    //boolean tieneDescuento = true;
+
+    //4//
+    //char letra = F
+
+    //5//
+    //int puntos = 10; {
+    //System.out.println(puntos);
+    //System.out.println(puntos + 10);
+//}
+    //6//
+   //double notaFinal = 8 ; {
+     //System.out.println(notaFinal >= 6 ? "Aprobado" : "Desaprobado");
+
+    //}
+
+    //7//
+    //double temperatura ; {
+    //temperatura = 36.6;
+    //System.out.println(double);
+    //}
+
+    //8//
+    //int 3hola = 1;
+    //error:
+    //Syntax error on token "3", delete this tokenJava(1610612968)
+
+    //9//
+    //int estudiante = 18;
+    //boolean mayorEdad = true;
+    //System.out.println("es mayor? " + mayorEdad);
+    //10//
+    //int x = 10; int y = 8; int z = 3;
+}
+
+
+// 1-
+// 2-
+// 3- que la variable declarada sin mas, no tiene nada asignado, pero dandole el valor, vale algo.
+// 4- es largo para hacer las cosas
+// 5- que se mantiene y la otra se puede modificar o alterar luego
+// 6- porque sino te da error y tambien porque no podrias continuar trabajando con esa misma
+// 7- 
+// 8- no sirve y la confunde
+// 9-
+// 10- int hola = 10; = el nombre solo a letras, el valor es entero y tiene ";" 
+// -   int jueves = 4; = lo mismo que antes 
+// -   String = 34; = el String es para cadena de texto
+// -   doble = 12; = el double es para decimales
